5692411aab55b2aeac5364911be0efdb30b8fb2b
Psycho Ben <psychoben@Psychos-MacBook-Pro.local>
2018-11-28 17:53:07 +0000

    adding default intellij project

---
+++ README.md
@@ -0,0 +1,1 @@
+#Default project steructure for IntelliJ
---
+++ src/test/java/TempTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TempTest {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(1,1);
+    }
+}
